3de71db36b0225ef6e908fe2887615154e43d458
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-04-05 11:23:21 +0530

    Remove package.json file and its dependencies

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 async function connectDB() {
     try{
-        await mongoose.connect(process.env.MONGOOSE_URL);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("DB CONNECTED");
     }
     catch(error){
