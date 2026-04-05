d6c5018b55b4037308f212a55619c3cd4856a903
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-04-05 10:59:41 +0530

    feat: add morgan middleware and update package.json scripts

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
-
+require("dotenv").config();
 
 connectDB();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,() => console.log(`server Up Baby at Port ${PORT}`));
 
